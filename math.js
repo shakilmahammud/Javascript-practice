328ddb35a46bdccae45f0b966e79930bac97a998
shakilmahammud <hshakil987@gmail.com>
2020-07-12 10:42:54 +0600

    math method practice work commit

---
+++ math.js
@@ -0,0 +1,26 @@
+//math method used
+
+var number=-10;
+var absoluteNumber=Math.abs(number);//math.abs function usde NEGATIVE  number convert to Positive number
+console.log(absoluteNumber);
+
+var number1=25.499999;
+var result=Math.round(number1);//math.round funntion used floaing number convert to integer. but math.round function when number 5.4999 then print 5 or 5.5 or 5.99 then always print 6.
+console.log(result);
+
+//math.clil function
+
+var number2  = 5.001;
+var resultNumber2=Math.ceil(number2);//this method print 6. math.ceil function alway increase floating number  one.
+console.log(resultNumber2);
+//math.floor function
+
+var number3=5.9999;
+var resultNumber3=Math.floor(number3);//math.floor method decrease floating number . point not count math.floor func always print figer interger.
+console.log(resultNumber3);
+
+//math.random function
+
+var number4=Math.random()*100;//math.random method print a random number always.
+Number4=Math.floor();
+console.log(number4);
