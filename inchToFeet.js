e9d319c1d94a4662e73d1acf1e08d8082822ec5e
shakilmahammud <hshakil987@gmail.com>
2020-07-14 10:36:49 +0600

    length func use cmmit

--- inchToFeet.js
+++ inchToFeet.js
@@ -2,3 +2,13 @@ function inchToFeet(inch){
     var feet=inch/12;
     return feet;
 }
+var sheFeet=inchToFeet(156);
+console.log(sheFeet);
+//length method  check how murch charecter in a wornd
+
+//ex
+var name="ami";
+if(name.length>2);{
+name="SHakil"
+}
+console.log(name);
