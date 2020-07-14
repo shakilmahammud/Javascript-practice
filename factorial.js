3ab8e1de142917baf3b9ed1c79f4e50b8dd7f9cf
shakilmahammud <hshakil987@gmail.com>
2020-07-14 16:13:22 +0600

    factorial math practice

---
+++ factorial.js
@@ -0,0 +1,20 @@
+//factorial math
+var factorial =1;
+for(var i=1; i<=100; i++){
+factorial = factorial*i;
+console.log(i,factorial)
+}
+
+//use function solve factorial math
+
+function factorialMath(num){
+    var factorials =1;
+    for(var i=1; i<=num; i++){
+    factorials = factorials*i;
+    
+}return factorials;
+    }
+const factorialResult=factorialMath(5);
+console.log(factorialResult);
+//use while loop solve factorial math 
+
