b07cdeb8e1af78801d9ff702424124c2b973c833
shakilmahammud <hshakil987@gmail.com>
2020-07-15 01:38:30 +0600

    Word Count Js

---
+++ wordCount.js
@@ -0,0 +1,24 @@
+var speech = "I'm a Good Person. I'm also Dedicated person ";
+var cout=0;
+for (var i=0; i<speech.length;i++){
+    var char=speech[i];
+    if(char==" " && speech[i-1] !=" "){
+        cout++;
+    }
+}
+console.log(cout);
+
+//use function
+
+function getCoutWord(speech){
+    var cout=0;
+    for (var i=0; i<speech.length;i++){
+        var char=speech[i];
+        if(char==" " && speech[i-1] !=" "){
+            cout++;
+        }
+    }
+    return cout;
+}
+var resultWord=getCoutWord(speech);
+console.log("Total Word:=",resultWord);
