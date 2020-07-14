0972646593f487c9663abf0525983d015f1c13dd
shakilmahammud <hshakil987@gmail.com>
2020-07-14 23:45:37 +0600

    max num

---
+++ max.js
@@ -0,0 +1,18 @@
+//who are big
+var business =450;
+var minister =350;
+var sochib =550;
+
+var max=Math.max(business,minister,sochib);
+console.log(max);
+/*
+if(business>minister){
+    if(business>sochib){
+        console.log("business is bigger");
+    }
+    else{
+        console.log("sochib is bigger")
+    }
+
+}
+*/
