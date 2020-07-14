9f24ad9ba92fcb403d49047ba1474a8205a17813
shakilmahammud <hshakil987@gmail.com>
2020-07-14 23:31:29 +0600

    random num js

---
+++ random.js
@@ -0,0 +1,12 @@
+var num =2.12458;
+var result = Math.floor(num);
+var result2 =Math.ceil(num);
+var result3 =Math.round(num);
+console.log(result);
+console.log(result2);
+console.log(result3);
+for(var i=0; i<10; i++){
+    var randomNum=Math.round(num)*100;
+    randomNum=Math.round(randomNum);
+    console.log(randomNum);
+}
