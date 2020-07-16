14eae555d529ff9886b59e2febf0b83cd2ccc752
shakilmahammud <hshakil987@gmail.com>
2020-07-16 12:00:57 +0600

    function js

---
+++ function.js
@@ -0,0 +1,23 @@
+//function use 
+
+/* function syntax :
+
+function NameOfFunction(parameter){
+    statement;
+}
+*/
+//function declear 
+function sayLoveYou(){
+    console.log("Jaan o baby");
+    console.log("hello");
+}
+sayLoveYou();//function call
+sayLoveYou();
+
+//use function parameter
+function dobuleItem(num){
+    var result=num*2;
+    return result;
+}
+var resultFunc=dobuleItem(100);
+console.log(resultFunc);
