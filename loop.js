681f71cc1a75eb07f82db6501b32c3b4bcdb531f
shakilmahammud <hshakil987@gmail.com>
2020-07-16 10:52:03 +0600

    loop js

---
+++ loop.js
@@ -0,0 +1,25 @@
+//while loop
+/*
+
+while synrax :
+variable declear 
+while(condition){
+    statemenet;
+    increment/deccrement;
+}
+*/
+var num=0;
+while(num<100){
+    console.log(num);
+    num++;
+}
+//for loop
+/*
+for loop syntax:
+for(variable declear;condition; increment/decrement;){
+    statement;
+}
+*/
+for(var i=0; i<=100; i++){
+    console.log(i);
+}
