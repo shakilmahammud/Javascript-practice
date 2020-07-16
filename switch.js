e35a3606b7701bc91ecab3d6c23183f927d6eb95
shakilmahammud <hshakil987@gmail.com>
2020-07-16 11:02:56 +0600

    switch js

---
+++ switch.js
@@ -0,0 +1,16 @@
+ num= 5;
+switch(num){
+    case 1000:
+        console.log("I'm 1000");
+        break;
+        case 100:
+        console.log("! m 100");
+        break;
+        case 20:
+        case 10:
+        console.log("I'm either 10 or 20");
+        break;
+        default:
+            console.log("I don't Know who are You");
+
+}
